efa63e5cef3ed5e7f76390440b3bbb98fd2de649
Andrea Gonteri <gonte@Gonte-MacBook-Pro.local>
2015-01-08 12:36:59 +0000

    Project creation

---
+++ iOS-mvvm/iOS-mvvm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS-mvvm
+//
+//  Created by andrea gonteri on 08/01/15.
+//  Copyright (c) 2015 CODR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS-mvvm/iOS-mvvm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS-mvvm
+//
+//  Created by andrea gonteri on 08/01/15.
+//  Copyright (c) 2015 CODR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
